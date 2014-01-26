f07b3a1bf21d8d99720a86a0a0257033bf26a4aa
Fernando <fndmarques@gmail.com>
2014-01-26 16:19:04 -0200

    Model

---
+++ Os/src/br/edu/ufabc/os/modelo/Os.java
@@ -0,0 +1,98 @@
+package br.edu.ufabc.os.modelo;
+
+@Entity
+@Table(name="os", schema="public")
+public class Os {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(name="os_DataAbertura")
+	private String os_DataAbertura;
+	@Column(name="empresa")
+	private String empresa;
+	@Column(name="nome")
+	private String nome;
+	@Column(name="telefone")
+	private String telefone;
+	@Column(name="email")
+	private String email;
+	@Column(name="os_Descricao")
+	private String os_Descricao;
+	@Column(name="os_Solucao")
+	private String os_Solucao;
+	@Column(name="os_DataAtendimento")
+	private String os_DataAtendimento;
+	@Column(name="os_DataConclusao")
+	private String os_DataConclusao;
+	@Column(name="os_Obs")
+	private String os_Obs;
+	
+	// getters e setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOs_DataAbertura() {
+		return os_DataAbertura;
+	}
+	public void setOs_DataAbertura(String os_DataAbertura) {
+		this.os_DataAbertura = os_DataAbertura;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getOs_Descricao() {
+		return os_Descricao;
+	}
+	public void setOs_Descricao(String os_Descricao) {
+		this.os_Descricao = os_Descricao;
+	}
+	public String getOs_Solucao() {
+		return os_Solucao;
+	}
+	public void setOs_Solucao(String os_Solucao) {
+		this.os_Solucao = os_Solucao;
+	}
+	public String getOs_DataAtendimento() {
+		return os_DataAtendimento;
+	}
+	public void setOs_DataAtendimento(String os_DataAtendimento) {
+		this.os_DataAtendimento = os_DataAtendimento;
+	}
+	public String getOs_DataConclusao() {
+		return os_DataConclusao;
+	}
+	public void setOs_DataConclusao(String os_DataConclusao) {
+		this.os_DataConclusao = os_DataConclusao;
+	}
+	public String getOs_Obs() {
+		return os_Obs;
+	}
+	public void setOs_Obs(String os_Obs) {
+		this.os_Obs = os_Obs;
+	}
+
+}
